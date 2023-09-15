6950dd024884aa4c0df947737365cb91e5ff8770
HiMAIayas <112191917+HiMAIayas@users.noreply.github.com>
2023-09-15 22:16:59 +0700

    Update and rename lab4_9.py to lab4_9 (1 line).py

---
+++ lab4 (if)/lab4_9 (1 line).py
@@ -0,0 +1,1 @@
+print(f"The charge is {20*int((int(input('Input parking time (minutes): '))+44)/60)} baht.")
--- lab4 (if)/lab4_9.py
+++
@@ -1,2 +0,0 @@
-time=int(input("Input parking time (minutes): "))
-print(f"The charge is {20*int((time+44)/60)} baht.")
