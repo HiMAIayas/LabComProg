e38930fde96699b6bdc6bc05c016a49f376932ab
HiMAIayas <112191917+HiMAIayas@users.noreply.github.com>
2023-09-15 18:05:03 +0700

    Delete 6622781407_lab5_1.py

--- 6622781407_lab5_1.py
+++
@@ -1,7 +0,0 @@
-even,odd=0,0
-for i in range(5):
-    num=int(input("Enter a number: "))
-    if num%2==0:even+=1
-    else:odd+=1
-print(f"There is/are {even} even numbers")
-print(f"There is/are {odd} odd numbers")
