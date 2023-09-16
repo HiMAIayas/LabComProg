81b376b17d4381d4f7a672433c9fbf0d971d0e9b
HiMAIayas <112191917+HiMAIayas@users.noreply.github.com>
2023-09-17 00:54:57 +0700

    Update prac_set1_7.py

--- labMidPractice/prac_set1_7.py
+++ labMidPractice/prac_set1_7.py
@@ -2,4 +2,5 @@ a =float(input("a = "))
 b =float(input("b = "))
 c =float(input("c = "))
 
-print(max(a,b,c))
+if a>0 and b>0 and c>0 and max(a,b,c) < a+b+c-max(a,b,c):print("Form a triangle")
+else:print("Not form a triangle")
